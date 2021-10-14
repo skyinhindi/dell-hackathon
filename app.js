e837bfc0aea49a327ca74fb8d40f936b0bdc028b
Aakash Chaudhary <aakash.199303185@muj.manipal.edu>
2021-10-14 20:53:07 +0530

    added filesystem reading

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 const express = require('express');
+const fs = require("fs");
+const decompress = require("decompress");
+const path = require("path");
+const xml2js = require("xml2js");
+const parser = new xml2js.Parser()
 
 const app = express();
 
@@ -9,6 +14,7 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
+
 app.get('/', (req, res) => {
     res.render('index');
 });
@@ -17,7 +23,27 @@ app.post('/search', (req, res) => {
   let serviceTags = req.body.serviceTags.split(',');
   let results = {};
   console.log(serviceTags);
-  //requires fetching data from file system and passing to the next page
+  serviceTags.forEach(tag => {
+    decompress(tag + '.zip', 'dist').then(files => {
+      //console.log(files);
+      files.forEach(file => {
+        fs.readFileSync("dist/" + file.path, (err, data) => {
+          if(err) 
+            console.log(err);
+          else{
+            parser.parseString(data, (error, result) => {
+              if(error) 
+                console.log(error)
+              else{
+                dat = Object.assign(result.root, dat);
+                console.log(dat);
+              }
+            })
+          }
+        });
+      });
+    });
+  });
   res.render('results', {results, serviceTags});
 });
 
@@ -26,5 +52,12 @@ app.use((req, res) => {
     res.status(404).render('404');
   });
 
+
+  fs.rmdir("dist", { recursive: true }, (err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("deleted!");
+  });
 
 app.listen(process.env.PORT || 3000);
